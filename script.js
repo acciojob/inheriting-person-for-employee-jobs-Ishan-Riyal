e5378e05c20da35246ae247de6668f6f734cded6
Ishan Riyal <ishanriyal07@gmail.com>
2025-07-30 12:58:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-// complete this js code
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 
 Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
-  Person.call(this, name, age); // Inherit name and age from Person
+  Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
-// Inherit from Person
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
